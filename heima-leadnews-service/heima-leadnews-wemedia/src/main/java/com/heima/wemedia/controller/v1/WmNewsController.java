3974af3ed283e358647ec119b9aeeda41c1e7cc9
sac <358329098@qq.com>
2023-01-05 18:34:36 +0800

    fun3-3-3:自媒体文章发布-自媒体文章管理-文章发布

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmNewsController.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmNewsController.java
@@ -1,11 +1,15 @@
 package com.heima.wemedia.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmNewsDto;
 import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
 import com.heima.wemedia.service.WmNewsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -14,8 +18,14 @@ public class WmNewsController {
     @Autowired
     private WmNewsService wmNewsService;
 
-   @PostMapping("/list")
+
+    @PostMapping("/list")
     public ResponseResult findAllNew(@RequestBody WmNewsPageReqDto dto) {
         return wmNewsService.findAllWmNew(dto);
+    }
+
+    @PostMapping("/submit")
+    public ResponseResult submitNews(@RequestBody WmNewsDto dto) {
+        return wmNewsService.submitNews(dto);
     }
 }
